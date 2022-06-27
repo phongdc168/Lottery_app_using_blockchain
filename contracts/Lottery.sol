89b63f0c18ca5cb53f79b8f2bd8690f7665758e4
Remix Workspace <>
2022-06-27 18:24:57 +0700

    add group player

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,12 +10,19 @@ contract Lottery {
     address public owner;
     uint public lotteryId;
     mapping (uint => address payable) public lotteryHistory;
+
+    struct ListNumberTicket{
+        address payable [] groupPlayer;
+    }
+    mapping (uint => ListNumberTicket) internal groupTicket;
+
     struct Participants {
         address payable player;
         uint numTicket;
     }
+    mapping(uint => Participants) internal allLottery;  
+
     uint internal playerCount = 0;
-    mapping(uint => Participants) internal AllLottery;  
     bytes32 internal keyHash; // identifies which Chainlink oracle to use
     uint internal fee;        // fee to get random number
     uint public randomResult;
@@ -28,7 +35,7 @@ contract Lottery {
     }
 
     function getPlayers(uint _index) public view returns (address payable, uint) {
-        Participants storage participant = AllLottery[_index];
+        Participants storage participant = allLottery[_index];
         return(
             participant.player,
             participant.numTicket
@@ -40,13 +47,23 @@ contract Lottery {
     }
     function enter(uint _numTicket) public payable {
         require(msg.value > 2 wei, "Not enough token");
-        require(_numTicket >= 1 && _numTicket <= 5, "Number ticket out of range");
-        Participants storage newPlayer = AllLottery[playerCount];
+        require(_numTicket >= 1 && _numTicket <= 10, "Number ticket out of range");
+        Participants storage newPlayer = allLottery[playerCount];
         newPlayer.player = payable(msg.sender);
         newPlayer.numTicket = _numTicket;
+        ListNumberTicket storage addTicket = groupTicket[newPlayer.numTicket];
+        addTicket.groupPlayer.push(payable(msg.sender));
         increasePlayerCount();
     }
 
+    function getAmountTicket(uint index) public view returns (uint, address[] memory){
+        address[] getListPlayer;
+        for(uint i = 0; i < groupTicket[index].groupPlayer.length; i++){
+            getListPlayer.push(groupTicket[index].groupPlayer[i]);
+        }
+        return (groupTicket[index].groupPlayer.length, getListPlayer);
+    }
+    // function getNumberTicket()
     // function payWinner() public {
     //     require(randomResult > 0, "Must have a source of randomness before choosing winner");
     //     uint index = randomResult % players.length;
